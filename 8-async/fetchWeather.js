4bfe5da7a1e492b6107a89e8f98f9d3e53646581
Максим Найдович <max.naid@mail.ru>
2019-08-14 11:53:39 +0500

    getWeather in 'async' function with getting data

--- 8-async/fetchWeather.js
+++ 8-async/fetchWeather.js
@@ -13,5 +13,35 @@ function getWeather(woeid) {
         .catch(error => console.log(error));
 }
 
-getWeather(44418);//London
-getWeather(2487956);//San Francisco
+//getWeather(44418);//London
+//getWeather(2487956);//San Francisco
+
+async function getWeatherAW(woeid) {
+    let resultMsg = 'everything is ';
+    try {
+        const result = await fetch
+        (`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`);
+        const data = await result.json();
+        const tomorrow = data.consolidated_weather[1];
+        console.log(`temperatures tomorrow in ${data.title} will stay between ${tomorrow.min_temp}°C and ${tomorrow.max_temp}°C`);
+        resultMsg += result.statusText;
+        return [resultMsg, data];
+    }catch (e) {
+        console.log(e);
+        resultMsg = 'something goes wrong';
+    }
+
+    return resultMsg;
+
+}
+let data; //записываем ресультат запроса в эту переменную
+getWeatherAW(44418).then(result => {
+    if (typeof result === "string") {
+        console.log(result);
+        return
+    }
+    console.log(result[0]);
+    data = result[1];
+    console.log(data);
+});
+// const dataSF = getWeatherAW(2487956).then(result => console.log(result)); - it's not working
